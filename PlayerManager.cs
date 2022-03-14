56c78dd129a3a2cff475ad2e4c6a5998761e93a0
WordlessSafe1 <94203733+WordlessSafe1@users.noreply.github.com>
2022-03-14 13:49:41 -0500

    Create Player Manager.

---
+++ PlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace BountyBot
+{
+    internal static class PlayerManager
+    {
+        //
+        private static readonly string recordPath = Directory.GetCurrentDirectory() + "\\bounties.dat";
+
+        // Fields
+        private static Dictionary<ulong,Player> players;
+        private static Player[] leaderboard;
+
+        // Properties
+        public static Dictionary<ulong, Player> Players { get => players; }
+        public static Player[] Leaderboard { get => leaderboard; }
+
+        // Functions
+        public static void AddPlayer(Player player) => players.Add(player.UID, player);
+
+        // JSON Functions
+        public static Dictionary<ulong, Player> LoadPlayers()
+        {
+            players = JsonSerializer.Deserialize<Dictionary<ulong, Player>>(File.ReadAllText(recordPath));
+            UpdateLeaderboard();
+            return players;
+        }
+        public static Player[] UpdateLeaderboard() => leaderboard = players.Values.OrderBy(x => x.Points).ToArray();
+        public static void SavePlayers() => File.WriteAllText(recordPath, JsonSerializer.Serialize(players));
+        public static void SavePlayers(Dictionary<ulong,Player> players) => File.WriteAllText(recordPath, JsonSerializer.Serialize(players));
+
+    }
+}
