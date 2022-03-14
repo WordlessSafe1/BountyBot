2d5c10bd70b2e63cca59310dc20469a5f7ee5fe4
WordlessSafe1 <94203733+WordlessSafe1@users.noreply.github.com>
2022-03-14 10:50:14 -0500

    Create Player definition.

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BountyBot
+{
+    internal class Player
+    {
+        // Fields
+        private readonly ulong uid;
+        private int[] bounties;
+        private int points;
+
+        // Properties
+        public ulong UID { get => uid; }
+        public int[] Bounties { get => bounties; }
+        public int Points { get => points; }
+
+        // Constructors
+        public Player(ulong uid) => this.uid = uid;
+        public Player(ulong uid, int[] bounties) => (this.uid, this.bounties) = (uid, bounties);
+        [System.Text.Json.Serialization.JsonConstructor]
+        public Player(ulong uid, int[] bounties, int points) => (this.uid, this.bounties, this.points) = (uid, bounties, points);
+
+        // Methods
+        public void AlterPoints(int pointsToAdd) => points += pointsToAdd;
+        public void ResetPoints() => points = 0;
+        public void AddBounty(int id) => bounties = bounties.Append(id).ToArray();
+        public void AddBounty(int[] ids) => bounties = bounties.Union(ids).ToArray();
+        public bool RemoveBounty(int id)
+        {
+            if (!bounties.Contains(id))
+                return false;
+            bounties = bounties.Where(x => x != id).ToArray();
+            return true;
+        }
+    }
+}
