5fd537eab7a083a90ce85d8a101aad7928784bc2
WordlessSafe1 <94203733+WordlessSafe1@users.noreply.github.com>
2022-04-09 22:19:38 -0500

    Vantiy fix.

--- Program.cs
+++ Program.cs
@@ -93,7 +93,7 @@ public class Program
         if (e.Exception is SlashExecutionChecksFailedException slex)
         {
             foreach (var check in slex.FailedChecks)
-                if (check is Attributes.RequireRoleAttribute att)
+                if (check is Attributes.RequireRoleAttribute)
                     await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(":x: **You do not have permission to run this command.**"));
         }
     }
